59db0654e51ea8150e2a133188675f9c0b8a4d7b
Agustin <90871968+Ler0v@users.noreply.github.com>
2022-09-20 23:47:46 -0300

    nav v1

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,8 +3,17 @@ import React from 'react'
 const Navbar = () => {
   return (
     <>
-        <nav className='nav'>
-
+        <img src="https://i.postimg.cc/B6MJCSb0/MUD-MAKE-UP-DESIGN-LOGO-BLANCO-Mesa-de-trabajo-1.png" className='imglogo' alt="i1" />
+        <nav>
+            <ul className='nav'>
+                <li>Inicio</li>
+                <li>Educacion</li>
+                <li>Trabajos</li>
+                <li>Nosotros</li>
+                <li>Productos</li>
+                <li>Servicios</li>
+                <li>Contacto</li>
+            </ul>
         </nav>
     </>
   )
