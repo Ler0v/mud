6b9c80a8a6b69adb0a9d9b6aa56a90e9623ac857
Agustin <90871968+Ler0v@users.noreply.github.com>
2022-09-21 00:10:59 -0300

    footer v0

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Navbar/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Navbar/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+        <div className='footerContainer'>
+            <div>
+            
+            </div>
+            <div>
+                <img src="https://i.postimg.cc/0yht1WdL/icons8-instagram-30.png" className='imgFooter' alt="ig" />
+                <img src="https://i.postimg.cc/YShhYR5q/icons8-facebook-30.png" className='imgFooter' alt="face" />
+            </div>
+        </div>
+        <h3 className='footerEnd'>© 2022 - MUD - Todos los derechos reservados</h3>
+    </>
+  )
+}
+
+export default Footer
