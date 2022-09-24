6ee3711661552c20c1c3347aff9d17a3d13e2516
Agustin <90871968+Ler0v@users.noreply.github.com>
2022-09-24 15:51:04 -0300

    navbar sin hecha sin animacion

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,33 +1,44 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import Menu from '../../assets/menu.svg'
+
 
 const Navbar = () => {
+
+  const [clicked, setClicked] = useState(false)
+
+  const handleClick = () => { 
+      setClicked(!clicked)
+  }
+
   return (
     <>
         <div className="navContainer">
           <nav>
               <Link to='/' className='navItem' ><img src="https://i.postimg.cc/B6MJCSb0/MUD-MAKE-UP-DESIGN-LOGO-BLANCO-Mesa-de-trabajo-1.png" className='imglogo' alt="i1" /></Link>
-              <ul className='nav'>
+              <img src={Menu} alt="menuIcon"  className='menuIcon active' onClick={handleClick}/>
+              <ul className={`nav ${clicked ? 'active' : ''}`}>
                   <li>
-                    <Link to='/' className='navItem'>Inicio</Link>
+                    <Link to='/' className='navItem' onClick={handleClick}>Inicio</Link>
                   </li>
                   <li>
-                    <Link to='/educacion' className='navItem'>Educacion</Link>
+                    <Link to='/educacion' className='navItem' onClick={handleClick}>Educacion</Link>
                   </li>
                   <li>
-                    <Link to='/trabajos' className='navItem'>Trabajos</Link>
+                    <Link to='/trabajos' className='navItem' onClick={handleClick}>Trabajos</Link>
                   </li>
                   <li>
-                    <Link to='/nosotros' className='navItem'>Nosotros</Link>
+                    <Link to='/nosotros' className='navItem' onClick={handleClick}>Nosotros</Link>
                   </li>
                   <li>
-                    <Link to='/productos' className='navItem'>Productos</Link>
+                    <Link to='/productos' className='navItem' onClick={handleClick}>Productos</Link>
                   </li>
                   <li>
-                    <Link to='/servicios' className='navItem'>Servicios</Link>
+                    <Link to='/servicios' className='navItem' onClick={handleClick} >Servicios</Link>
                   </li>
                   <li>
-                    <Link to='/contacto' className='navItem'>Contacto</Link>
+                    <Link to='/contacto' className='navItem' onClick={handleClick}>Contacto</Link>
                   </li>
               </ul>
           </nav>
--- src/components/Navbar/Paginas/Inicio.jsx
+++ src/components/Navbar/Paginas/Inicio.jsx
@@ -4,19 +4,24 @@ import videoInicio from '../../../assets/videoInicio.mp4'
 const Inicio = () => {
   return (
     <>
-      <img src="https://i.postimg.cc/B6nVgBym/1.jpg" className='imgIndex' alt="imgBody" />
       <video src={videoInicio} autoPlay loop muted className='videoBody' />
+      <img src="https://i.postimg.cc/B6nVgBym/1.jpg" className='imgIndex' alt="imgBody" />
       <div className='mudAnimado'>
         <h1>M</h1>
         <h1>U</h1>
         <h1>D</h1>
       </div>
-      <h1 className='tituloBody'>Nuestros trabajos</h1>
       <div className='cardsBody'>
+        <h1 className='letraIndex'>Nuestros servicios</h1>
         <img src="https://i.postimg.cc/Kv0vyGFs/IMG-1265.jpg" className='card1' alt="imgB1" />
+      </div>
+      <div className='cardsBody'>
+        <h1 className='letraIndex'>Nuestros servicios</h1>
         <img src="https://i.postimg.cc/C1pw4dHd/IMG-3807.jpg" className='card2' alt="imgB2" />
+      </div>
+      <div className='cardsBody'>
+        <h1 className='letraIndex'>Nuestros servicios</h1>
         <img src="https://i.postimg.cc/fTsnq69q/1edit.jpg" className='card3' alt="imgB3" />
-        <img src="https://i.postimg.cc/fTsnq69q/1edit.jpg" className='card4' alt="imgB4" />
       </div>
     </>
   )
