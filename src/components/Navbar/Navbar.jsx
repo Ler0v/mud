1041184e56bf921a5c770e28f01531ccdbea7839
Agustin <90871968+Ler0v@users.noreply.github.com>
2022-09-27 08:41:33 -0300

    cambio nav

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -19,29 +19,31 @@ const Navbar = () => {
           <nav>
               <Link to='/' className='navItem' ><img src="https://i.postimg.cc/B6MJCSb0/MUD-MAKE-UP-DESIGN-LOGO-BLANCO-Mesa-de-trabajo-1.png" className='imglogo' alt="i1" /></Link>
               <img src={Menu} alt="menuIcon"  className='menuIcon active' onClick={handleClick}/>
-              <ul className={`nav ${clicked ? 'active' : ''}`}>
-                  <li>
-                    <Link to='/' className='navItem' onClick={handleClick}>Inicio</Link>
-                  </li>
-                  <li>
-                    <Link to='/servicios' className='navItem' onClick={handleClick}>Servicios</Link>
-                  </li>
-                  <li>
-                    <Link to='/trabajos' className='navItem' onClick={handleClick}>Trabajos</Link>
-                  </li>
-                  <li>
-                    <Link to='/nosotros' className='navItem' onClick={handleClick}>Nosotros</Link>
-                  </li>
-                  <li>
-                    <Link to='/productos' className='navItem' onClick={handleClick}>Productos</Link>
-                  </li>
-                  <li>
-                    <Link to='/educacion' className='navItem' onClick={handleClick} >Carreras</Link>
-                  </li>
-                  <li>
-                    <Link to='/contacto' className='navItem' onClick={handleClick}>Contacto</Link>
-                  </li>
-              </ul>
+              <div>
+                <ul className={`nav ${clicked ? 'active' : ''}`}>
+                    <li>
+                      <Link to='/' className='navItem' onClick={handleClick}>Inicio</Link>
+                    </li>
+                    <li>
+                      <Link to='/servicios' className='navItem' onClick={handleClick}>Servicios</Link>
+                    </li>
+                    <li>
+                      <Link to='/trabajos' className='navItem' onClick={handleClick}>Trabajos</Link>
+                    </li>
+                    <li>
+                      <Link to='/nosotros' className='navItem' onClick={handleClick}>Nosotros</Link>
+                    </li>
+                    <li>
+                      <Link to='/productos' className='navItem' onClick={handleClick}>Productos</Link>
+                    </li>
+                    <li>
+                      <Link to='/educacion' className='navItem' onClick={handleClick} >Carreras</Link>
+                    </li>
+                    <li>
+                      <Link to='/contacto' className='navItem' onClick={handleClick}>Contacto</Link>
+                    </li>
+                </ul>
+              </div>
           </nav>
         </div>
     </>
