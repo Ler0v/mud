b50856a355825739c9297d314334e642bb4d2e07
Agustin <90871968+Ler0v@users.noreply.github.com>
2022-09-28 23:15:07 -0300

    servicios modelado

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -14,39 +14,37 @@ const Navbar = () => {
   }
 
   return (
-    <>
-        <div className="navContainer">
-          <nav>
-              <Link to='/' className='navItem' ><img src="https://i.postimg.cc/B6MJCSb0/MUD-MAKE-UP-DESIGN-LOGO-BLANCO-Mesa-de-trabajo-1.png" className='imglogo' alt="i1" /></Link>
-              <img src={Menu} alt="menuIcon"  className='menuIcon active' onClick={handleClick}/>
-              <div>
-                <ul className={`nav ${clicked ? 'active' : ''}`}>
-                    <li>
-                      <Link to='/' className='navItem' onClick={handleClick}>Inicio</Link>
-                    </li>
-                    <li>
-                      <Link to='/servicios' className='navItem' onClick={handleClick}>Servicios</Link>
-                    </li>
-                    <li>
-                      <Link to='/trabajos' className='navItem' onClick={handleClick}>Trabajos</Link>
-                    </li>
-                    <li>
-                      <Link to='/nosotros' className='navItem' onClick={handleClick}>Nosotros</Link>
-                    </li>
-                    <li>
-                      <Link to='/productos' className='navItem' onClick={handleClick}>Productos</Link>
-                    </li>
-                    <li>
-                      <Link to='/educacion' className='navItem' onClick={handleClick} >Carreras</Link>
-                    </li>
-                    <li>
-                      <Link to='/contacto' className='navItem' onClick={handleClick}>Contacto</Link>
-                    </li>
-                </ul>
-              </div>
-          </nav>
-        </div>
-    </>
+    <div className="navContainer">
+      <nav>
+          <Link to='/' className='navItem' ><img src="https://i.postimg.cc/B6MJCSb0/MUD-MAKE-UP-DESIGN-LOGO-BLANCO-Mesa-de-trabajo-1.png" className='imglogo' alt="i1" /></Link>
+          <img src={Menu} alt="menuIcon"  className='menuIcon active' onClick={handleClick}/>
+          <div>
+            <ul className={`nav ${clicked ? 'active slide-in-top' : ''}`}>
+                <li>
+                  <Link to='/' className='navItem' onClick={handleClick}>Inicio</Link>
+                </li>
+                <li>
+                  <Link to='/servicios' className='navItem' onClick={handleClick}>Servicios</Link>
+                </li>
+                <li>
+                  <Link to='/trabajos' className='navItem' onClick={handleClick}>Trabajos</Link>
+                </li>
+                <li>
+                  <Link to='/nosotros' className='navItem' onClick={handleClick}>Nosotros</Link>
+                </li>
+                <li>
+                  <Link to='/productos' className='navItem' onClick={handleClick}>Productos</Link>
+                </li>
+                <li>
+                  <Link to='/educacion' className='navItem' onClick={handleClick} >Carreras</Link>
+                </li>
+                <li>
+                  <Link to='/contacto' className='navItem' onClick={handleClick}>Contacto</Link>
+                </li>
+            </ul>
+          </div>
+      </nav>
+    </div>
   )
 }
 
--- src/containers/Servicios/Servicios.jsx
+++ src/containers/Servicios/Servicios.jsx
@@ -1,13 +1,96 @@
 import React from 'react'
 import './servicios.css'
+import Carousel from './Carousel'
+
 
 const Servicios = () => {
   return (
     <>
-        
-            <div className='serviciosContainer'>
-                <h1 className='serviciosTitulo'>Servicios</h1>
+    <div className='serviciosContainer'>
+      <h1 className='serviciosTitulo'>Servicios</h1>
+      <div className='serviciosContainerItem'> 
+        <div className="serviciosContainerItem1">
+          <div className='servicioGuia'>
+            <p className='tituloItem'>PRO MAKEUP & HAIRSTYLE</p>
+            <button>Ver mas</button>
+          </div>
+        </div>
+        <div className="serviciosContainerItem2">
+          <div className='servicioGuia'>
+            <p className='tituloItem'>Cejas</p>
+            <button>Ver mas</button>
+          </div>
+        </div>
+        <div className="serviciosContainerItem3">
+          <div className='servicioGuia'>
+              <p className='tituloItem'>Pestanias</p>
+              <button>Ver mas</button>
             </div>
+        </div>
+        <div className="serviciosContainerItem4">
+          <div className='servicioGuia'>
+              <p className='tituloItem'>MUD PRO NAILS</p>
+              <button>Ver mas</button>
+            </div>
+        </div>
+      </div>
+    </div>
+    <div className='servicio'>
+      <div className='servicioCarousel'>
+        <Carousel />
+      </div>
+      <div className='servicioInfo'>
+        <h1>Pro Make Up & Hairstyle</h1>
+        <div className='servicioBotones'>
+          <button>Pro Makeup</button>
+          <button>Bodas</button>
+          <button>15 Años</button>
+          <button>Pro Hairstyle</button>
+        </div>
+      </div>
+    </div>
+    <div className='servicio'>
+      <div className='servicioCarousel'>
+        <Carousel />
+      </div>
+      <div className='servicioInfo'>
+        <h1>Cejas</h1>
+        <div className='servicioBotones'>
+          <button>Full Brows Design</button>
+          <button>Brow Service</button>
+          <button>Brow Lamination</button>
+          <button>Brow Tint</button>
+          <button>Hair Botox Revitalize</button>
+          <button>Microblading</button>
+        </div>
+      </div>
+    </div>
+    <div className='servicio'>
+      <div className='servicioCarousel'>
+        <Carousel />
+      </div>
+      <div className='servicioInfo'>
+        <h1>Pestanias</h1>
+        <div className='servicioBotones'>
+          <button>Lash Lifting</button>
+          <button>Hair Botox Revitalize</button>
+        </div>
+      </div>
+    </div>
+    <div className='servicio'>
+      <div className='servicioCarousel'>
+        <Carousel />
+      </div>
+      <div className='servicioInfo'>
+        <h1>MUD Pro Nails</h1>
+        <div className='servicioBotones'>
+          <button>Manicuría combinada</button>
+          <button>Manicuría combinada con esmaltado tradicional/semipermanente</button>
+          <button>Capping gel</button>
+          <button>Press on nails</button>
+        </div>
+      </div>
+    </div>
     </>
   )
 }
