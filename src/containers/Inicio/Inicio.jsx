69a1f15f8fbb3ea2ae3f86ffde920c660168bc7c
Agustin <90871968+Ler0v@users.noreply.github.com>
2022-10-06 22:41:30 -0300

    Inicio casi terminado

--- src/containers/Inicio/Inicio.jsx
+++ src/containers/Inicio/Inicio.jsx
@@ -14,21 +14,41 @@ const Inicio = () => {
         <p className='letraAnimada'>U</p>
         <p className='letraAnimada'>D</p>
       </div>
-      <div className='cardsBody'>
-        <h1 className='letraIndex'>Nuestros servicios</h1>
-        <Link to='/servicios'><img src="https://i.postimg.cc/Kv0vyGFs/IMG-1265.jpg" className='card1' alt="imgB1" /></Link>
-      </div>
-      <div className='cardsBody'>
-        <h1 className='letraIndex'>Nuestras carreras</h1>
-        <Link to='/carreras'><img src="https://i.postimg.cc/C1pw4dHd/IMG-3807.jpg" className='card2' alt="imgB2" /></Link>
-      </div>
-      <div className='cardsBody'>
-        <h1 className='letraIndex'>Nuestros productos</h1>
-        <Link to='/productos'><img src="https://i.postimg.cc/fTsnq69q/1edit.jpg" className='card3' alt="imgB3" /></Link>
+      <div className='cardsBodyContainer'>
+        <h1>Porque mud?</h1>
+        <div className="cardsBody">
+          <div className='cardBodyContainer'>
+            <h1 className='letraIndex'>Nuestros servicios</h1>
+            <Link to='/servicios'><img src="https://i.postimg.cc/Kv0vyGFs/IMG-1265.jpg" className='card1' alt="imgB1" /></Link>
+          </div>
+          <div className='cardBodyContainer'>
+            <h1 className='letraIndex'>Nuestras carreras</h1>
+            <Link to='/carreras'><img src="https://i.postimg.cc/C1pw4dHd/IMG-3807.jpg" className='card2' alt="imgB2" /></Link>
+          </div>
+          <div className='cardBodyContainer'>
+            <h1 className='letraIndex'>Nuestros productos</h1>
+             <Link to='/productos'><img src="https://i.postimg.cc/fTsnq69q/1edit.jpg" className='card3' alt="imgB3" /></Link>
+          </div>     
+        </div>
       </div>
       <div className="aboutIndex">
-        <p>MUD nació hace 15 años como un proyecto creativo de Celeste González, directora de la compañía.</p>
-        <img src="https://makeupdesign.com.ar/img/nosotros/celeste.jpg" alt="aboutIndexfoto" />
+        <img src="https://i.postimg.cc/zGPWMDtQ/IMG-6398.jpg" alt="aboutIndexfoto" />
+        <div className='aboutIndexInfo'>
+          <h1>Nuestros trabajos</h1>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet minima cupiditate quos hic repellat corrupti nam neque molestias deleniti esse saepe animi, doloremque quam in maxime? Omnis accusamus veniam velit.</p>
+          <Link to='/trabajos'><button>Ver Trabajos</button></Link>
+        </div>
+      </div>
+      <div className='contactoInicioContainer'>
+        <div className='contactoInicioInfo'>
+          <div>
+            <h1>Te intereso nuestro trabajo?</h1>
+            <h3>Contactanos!</h3>
+          </div>
+          <div>
+            <Link to='/trabajos' className='contactoInicioButton'><button>Ver Trabajos</button></Link>
+          </div>
+        </div>
       </div>
     </>
   )
