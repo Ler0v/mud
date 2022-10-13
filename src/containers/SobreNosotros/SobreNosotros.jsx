b432af0a9c7720c4ec37f4fdb8db5fdeaab58d10
Agustin <90871968+Ler0v@users.noreply.github.com>
2022-10-12 23:31:55 -0300

    nosotros modelado

--- src/containers/SobreNosotros/SobreNosotros.jsx
+++ src/containers/SobreNosotros/SobreNosotros.jsx
@@ -4,14 +4,20 @@ import './sobrenosotros.css'
 const SobreNosotros = () => {
   return (
     <div className='sobreNosotrosContainer'>
-        <div className="sobreNosotros1">
-            <div className='imgFondo1'>
-                <img src="https://i.postimg.cc/B6MJCSb0/MUD-MAKE-UP-DESIGN-LOGO-BLANCO-Mesa-de-trabajo-1.png" alt="imgSobreNosotros" />
-            </div>
-            <div className='tituloSobreNosotros'>
-                <h1>"MY SECRET IS MY FORMULAS. I PERSONALLY DESIGN EVERY PRODUCT FROM START TO FINISH USING ONLY THE HIGHEST-QUALITY INGREDIENTS IN THE WORLD."</h1>
-            </div>
-        </div>
+      <div className="sobreNosotros1">
+          <div className='imgFondo'>
+              <img src="https://i.postimg.cc/B6MJCSb0/MUD-MAKE-UP-DESIGN-LOGO-BLANCO-Mesa-de-trabajo-1.png" className='imgFondo1'alt="imgSobreNosotros" />
+              <img src="https://i.postimg.cc/8Pjwc3xx/IMG-6398-1.png" className='imgFondo2' alt="imgNosotros2" />
+          </div>
+          <div className='tituloSobreNosotros'>
+              <h1>"MY SECRET IS MY FORMULAS. I PERSONALLY DESIGN EVERY PRODUCT FROM START TO FINISH USING ONLY THE HIGHEST-QUALITY INGREDIENTS IN THE WORLD."</h1>
+          </div>
+      </div>
+      <div className='textosMotivacionalesSN'>
+        <h1 className='textoMotivacionesItem'>Born in Croatia and raised in Germany, Natasha inherited a love for both painting and science from her mother, a technical illustrator and chemist who would often bring her to the lab.</h1>
+        <h1 className='textoMotivacionesItem'>Born in Croatia and raised in Germany, Natasha inherited a love for both painting and science from her mother, a technical illustrator and chemist who would often bring her to the lab.</h1>
+        <h1 className='textoMotivacionesItem'>Born in Croatia and raised in Germany, Natasha inherited a love for both painting and science from her mother, a technical illustrator and chemist who would often bring her to the lab.</h1>
+      </div>
     </div>
 
   )
