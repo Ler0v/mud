6988c8437033ef80fddb654bb3e9e2d8a2cf9629
Agustin <90871968+Ler0v@users.noreply.github.com>
2022-10-13 23:50:58 -0300

    Sobre nosotros modelado bien pensado

--- src/containers/SobreNosotros/SobreNosotros.jsx
+++ src/containers/SobreNosotros/SobreNosotros.jsx
@@ -5,18 +5,32 @@ const SobreNosotros = () => {
   return (
     <div className='sobreNosotrosContainer'>
       <div className="sobreNosotros1">
-          <div className='imgFondo'>
-              <img src="https://i.postimg.cc/B6MJCSb0/MUD-MAKE-UP-DESIGN-LOGO-BLANCO-Mesa-de-trabajo-1.png" className='imgFondo1'alt="imgSobreNosotros" />
-              <img src="https://i.postimg.cc/8Pjwc3xx/IMG-6398-1.png" className='imgFondo2' alt="imgNosotros2" />
-          </div>
-          <div className='tituloSobreNosotros'>
-              <h1>"MY SECRET IS MY FORMULAS. I PERSONALLY DESIGN EVERY PRODUCT FROM START TO FINISH USING ONLY THE HIGHEST-QUALITY INGREDIENTS IN THE WORLD."</h1>
-          </div>
+        <div className='imgFondo'>
+          <img src="https://i.postimg.cc/B6MJCSb0/MUD-MAKE-UP-DESIGN-LOGO-BLANCO-Mesa-de-trabajo-1.png" className='imgFondo1'alt="imgSobreNosotros" />
+          <img src="https://i.postimg.cc/8Pjwc3xx/IMG-6398-1.png" className='imgFondo2' alt="imgNosotros2" />
+        </div>
+        <div className='tituloSobreNosotros'>
+          <h1>"MY SECRET IS MY FORMULAS. I PERSONALLY DESIGN EVERY PRODUCT FROM START TO FINISH USING ONLY THE HIGHEST-QUALITY INGREDIENTS IN THE WORLD."</h1>
+        </div>
       </div>
-      <div className='textosMotivacionalesSN'>
-        <h1 className='textoMotivacionesItem'>Born in Croatia and raised in Germany, Natasha inherited a love for both painting and science from her mother, a technical illustrator and chemist who would often bring her to the lab.</h1>
-        <h1 className='textoMotivacionesItem'>Born in Croatia and raised in Germany, Natasha inherited a love for both painting and science from her mother, a technical illustrator and chemist who would often bring her to the lab.</h1>
-        <h1 className='textoMotivacionesItem'>Born in Croatia and raised in Germany, Natasha inherited a love for both painting and science from her mother, a technical illustrator and chemist who would often bring her to the lab.</h1>
+      <div className='textosMotivacionalesSNContainer'>
+        <h1>Born in Croatia and raised in Germany, Natasha inherited a love for both painting and science from her mother.</h1>
+        <h1>Born in Croatia and raised in Germany, Natasha inherited a love for both painting and science from her mother.</h1>
+      </div>
+      <div className="historiaSN1"> 
+        <h1 className='historiaSN1Item1'>Celeste Gonzales</h1>
+        <img src="https://i.postimg.cc/RCfvWXXR/61-de40b12e563144010116591215845626-480-0.jpg" className='historiaSN1Item2 imgCeles1' alt="imgSobreNosotros3" />
+        <p className='historiaSN1Item3'>Born in Croatia and raised in Germany, Natasha inherited a love for both painting and science from her mother, a technical illustrator and chemist who would often bring her to the lab.
+        Born in Croatia and raised in Germany, Natasha inherited a love for both painting and science from her mother, a technical illustrator and chemist who would often bring her to the lab.
+        Born in Croatia and raised in Germany, Natasha inherited a love for both painting and science from her mother, a technical illustrator and chemist who would often bring her to the lab.</p>
+        <img src="https://i.postimg.cc/66C6Tt0q/6.jpg" className='historiaSN1Item4 imgCeles2' alt="imgSobreNosotros4" />
+      </div>
+      <div className="mudAnimadoNosotros">
+        <p>MUD MUD MUD MUD MUD MUD MUD MUD MUD MUD MUD MUD MUD MUD MUD MUD MUD MUD MUD MUD</p>
+      </div>
+      <div>
+      <h1>Born in Croatia and raised in Germany, Natasha inherited a love for both painting and science from her mother, a technical illustrator and chemist who would often bring her to the lab.</h1>
+      <img src="https://i.postimg.cc/RCfvWXXR/61-de40b12e563144010116591215845626-480-0.jpg" alt="imgSobreNosotros3" />
       </div>
     </div>
 
