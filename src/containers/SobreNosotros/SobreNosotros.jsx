24bc2e7f0af11f6fe5399ae2e3cbd9e3ad9870fe
Agustin <90871968+Ler0v@users.noreply.github.com>
2022-11-14 20:28:57 -0300

    nosotros idea animacion

--- src/containers/SobreNosotros/SobreNosotros.jsx
+++ src/containers/SobreNosotros/SobreNosotros.jsx
@@ -14,10 +14,15 @@ const SobreNosotros = () => {
           animate={{x: 0}}
           transition={{duration:2}}
           />
-          <img src="https://i.postimg.cc/8Pjwc3xx/IMG-6398-1.png" className='imgFondo2' alt="imgNosotros2" />
+          <motion.img src="https://i.postimg.cc/8Pjwc3xx/IMG-6398-1.png" className='imgFondo2' alt="imgNosotros2" 
+          initial={{x: 400}}
+          animate={{x:0}}
+          transition={{duration: 2}}
+          />
         </div>
         <div className='tituloSobreNosotros'>
-          <h1>"Como maquilladora pude alcanzar los sucesos más importantes, tapas de revistas, los desfiles de moda más importantes, celebrities."</h1>
+          <motion.h1
+          >"Como maquilladora pude alcanzar los sucesos más importantes, tapas de revistas, los desfiles de moda más importantes, celebrities."</motion.h1>
         </div>
       </div>
       <div className='textosMotivacionalesSNContainer'>
@@ -26,20 +31,31 @@ const SobreNosotros = () => {
       </div>
       <div className="historiaSN1"> 
         <h1 className='historiaSN1Item1'>Celeste Gonzales</h1>
-        <img src="https://i.postimg.cc/RCfvWXXR/61-de40b12e563144010116591215845626-480-0.jpg" className='historiaSN1Item2 imgCeles1' alt="imgSobreNosotros3" />
+        <motion.img src="https://i.postimg.cc/RCfvWXXR/61-de40b12e563144010116591215845626-480-0.jpg" className='historiaSN1Item2 imgCeles1' alt="imgSobreNosotros3"
+        initial={{x: -200}}
+        animate={{x:0}}
+        transition={{duration: 2}}
+        />
         <div className='historiaSN1Item3'>
           <p>MUD nació hace 15 años como un proyecto creativo de Celeste González, directora de la compañía.</p>
           <p>Su carrera comenzó hace 20 años, tuvo sus inicios en las grandes compañías de cosmética internacional tales como Natura y Lancome. Luego de unos años en compañías internacioles y fortalecer su carrera profesional maquillallando en producciones, desfiles y a grandes figures del espectáculo a nivel internacional nació MUD.</p>
           <p>Cuenta con un sinfín de cursos y seminarios a nivel internacional que avalan su expertise en el área, trabajo con grandes fotógrafos, diseñadores y estilistas de talla internacional.</p>
           <p>Entre su portafolio se encuentras figuras como: Sofía Zámolo, Julieta Prandi, Paula Chaves, Zaira Nara, Barbie Vélez.</p>
         </div>
-        <img src="https://i.postimg.cc/66C6Tt0q/6.jpg" className='historiaSN1Item4 imgCeles2' alt="imgSobreNosotros4" />
+        <motion.img src="https://i.postimg.cc/66C6Tt0q/6.jpg" className='historiaSN1Item4 imgCeles2' alt="imgSobreNosotros4" 
+        initial={{x:400}}
+        animate={{x:0}}
+        transition={{duration: 2}}
+        />
       </div>
       <div className="mudAnimadoNosotros">
         <p>MUD MUD MUD MUD MUD MUD MUD MUD MUD MUD MUD MUD MUD MUD MUD MUD MUD MUD MUD MUD MUD MUD MUD MUD MUD MUD MUD MUD MUD MUD</p>
       </div>
       <div className='historiaSN2'>
-        <img src="https://i.postimg.cc/RCfvWXXR/61-de40b12e563144010116591215845626-480-0.jpg" alt="imgSobreNosotros3" />
+        <motion.img src="https://i.postimg.cc/RCfvWXXR/61-de40b12e563144010116591215845626-480-0.jpg" alt="imgSobreNosotros3"
+         initial={{x: -200}}
+         animate={{x:0}}
+         transition={{duration: 2}} />
         <p>MUD comenzó con la educación como eje principal para formar profesionales calificados para trabajar en una industria competitiva. Sumado a sus academias, MUD cuenta con tiendas en donde se realizan servicios de belleza.</p>
         <p>El último proyecto de MUD es el lanzamiento de su propia línea de labiales y esmaltes diseñados especialmente por la directora de MUD respetando altos estándares internacionales, los cuales se comercializan en sus tiendas físicas y se distribuyen a todo el país a través de tu e-comerce.</p>
       </div>
