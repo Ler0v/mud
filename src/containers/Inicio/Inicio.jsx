ab813c46c234b96a1b7df985c856a356efdf5e36
Agustin <90871968+Ler0v@users.noreply.github.com>
2022-11-17 16:32:08 -0300

    inicio animado

--- src/containers/Inicio/Inicio.jsx
+++ src/containers/Inicio/Inicio.jsx
@@ -2,38 +2,75 @@ import React from 'react'
 import './inicio.css'
 import videoInicio from '../../assets/videoInicio.mp4'
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+
+
+const animacionOnScreen = {
+  offScreen: {opacity:0}, 
+  onScreen: {opacity:1}
+}
 
 
 const Inicio = () => {
   return (
     <>
       <div className='videoBody'>
-        <video className='videoBodyItem' src={videoInicio} autoPlay loop muted />
+        <motion.video className='videoBodyItem' src={videoInicio} autoPlay loop muted
+          initial={"offScreen"}
+          whileInView={"onScreen"}
+          viewport={{once: true}}
+          transition={{duration:2}}
+          variants={animacionOnScreen}
+        />
       </div>
-      {/* <div className='mudAnimado'>
-        <p className='letraAnimada'>M</p>
-        <p className='letraAnimada'>U</p>
-        <p className='letraAnimada'>D</p>
-      </div> */}
       <div className='cardsBodyContainer'>
-        <h1 className='tituloCardsBody'>Porque MUD?</h1>
-        <div className="cardsBody">
+        <motion.h1
+          initial={"offScreen"}
+          whileInView={"onScreen"}
+          viewport={{once: true,amount:0.7}}
+          transition={{duration:2}}
+          variants={animacionOnScreen}
+        className='tituloCardsBody'>Porque MUD?</motion.h1>
+        <motion.div className="cardsBody"
+          initial={"offScreen"}
+          whileInView={"onScreen"}
+          viewport={{once: true,amount:0.5}}
+          transition={{duration:2}}
+          variants={animacionOnScreen}
+        >
           <div className='cardBodyContainer'>
             <h1 className='letraIndex'>Nuestros servicios</h1>
-            <Link to='/servicios'><img src="https://i.postimg.cc/Kv0vyGFs/IMG-1265.jpg" className='card1' alt="imgB1" /></Link>
+            <Link to='/servicios'><motion.img
+              initial={{scale:1}}
+              whileHover={{scale:1.1}}
+              transition={{duration:1}}            
+            src="https://i.postimg.cc/Kv0vyGFs/IMG-1265.jpg" className='card1' alt="imgB1" /></Link>
           </div>
           <div className='cardBodyContainer'>
             <h1 className='letraIndex'>Nuestras carreras</h1>
-            <Link to='/carreras'><img src="https://i.postimg.cc/C1pw4dHd/IMG-3807.jpg" className='card2' alt="imgB2" /></Link>
+            <Link to='/carreras'><motion.img 
+              initial={{scale:1}}
+              whileHover={{scale:1.1}}
+              transition={{duration:1}}  
+            src="https://i.postimg.cc/C1pw4dHd/IMG-3807.jpg" className='card2' alt="imgB2" /></Link>
           </div>
           <div className='cardBodyContainer'>
             <h1 className='letraIndex'>Nuestros productos</h1>
-             <Link to='/productos'><img src="https://i.postimg.cc/fTsnq69q/1edit.jpg" className='card3' alt="imgB3" /></Link>
+             <Link to='/productos'><motion.img
+              initial={{scale:1}}
+              whileHover={{scale:1.1}}
+              transition={{duration:1}}  
+             src="https://i.postimg.cc/fTsnq69q/1edit.jpg" className='card3' alt="imgB3" /></Link>
           </div>     
-        </div>
+        </motion.div>
       </div>
       <div className="aboutIndex">
-        <img src="https://i.postimg.cc/zGPWMDtQ/IMG-6398.jpg" alt="aboutIndexfoto" />
+        <motion.img src="https://i.postimg.cc/zGPWMDtQ/IMG-6398.jpg" alt="aboutIndexfoto"
+        initial={{opacity:0}}
+        whileInView={{opacity:1}}
+        viewport={{once:true,amount:0.5}}
+        transition={{duration:3}}
+        />
         <div className='aboutIndexInfo'>
           <h1>Nuestros trabajos</h1>
           <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet minima cupiditate quos hic repellat corrupti nam neque molestias deleniti esse saepe animi, doloremque quam in maxime? Omnis accusamus veniam velit.</p>
