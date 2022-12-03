dbc9c2e6dea9b940f99f9cb1b3a823571c675edd
Agustin <90871968+Ler0v@users.noreply.github.com>
2022-12-03 12:20:07 -0300

    servicios sin cards mobile

--- src/containers/Servicios/Servicios.jsx
+++ src/containers/Servicios/Servicios.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 import './servicios.css'
 import Carousel from './Carousel'
 import {motion} from 'framer-motion'
+import { useState } from 'react'
+
 
 const Servicios = () => {
+
+  const [info ,setInfo] = useState("")
+
   return (
     <>
       <div className='serviciosContainer'>
@@ -58,10 +63,13 @@ const Servicios = () => {
         <div className='servicioInfo' >
           <h1>Pro Make Up & Hairstyle</h1>
           <div className='servicioBotones'>
-            <button>Pro Makeup</button>
-            <button>Bodas</button>
+            <button onClick={()=> setInfo("El servicio estrella de MUD. Nos dedicamos hace mas de 13 años a resaltar la belleza natural de las personas a través del arte del maquillaje. Nuestro objetivo es hacerte sentir bella, cómoda y natural. Utilizamos productos de primeras marcas, importados y nacionales, y las técnicas más novedosas y trend de la industria. Si tenes un evento en el cual queres lucirte, elegí maquillarte con profesionales capacitadas, elegi MUD.")}>Pro Makeup</button>
+            <button onClick={()=> setInfo("Elegí tu Make-up ArtistEn MUD te ofrecemos dos opciones:Maquillaje con Staff: contamos con un staff de artistas del maquillaje altamente capacitadas/os, quienes se perfeccionan constantemente y cuentan con las más novedosas técnicas y productos del mercado.")}>Bodas</button>
             <button>15 Años</button>
             <button>Pro Hairstyle</button>
+          </div>
+          <div className='infoServicios'>
+            {info}
           </div>
         </div>
       </div>
