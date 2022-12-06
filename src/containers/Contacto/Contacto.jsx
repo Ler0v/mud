474a093b429990ab00c018d1af193c5e6bba8522
Agustin <90871968+Ler0v@users.noreply.github.com>
2022-12-05 22:41:09 -0300

    email js funcionando

--- src/containers/Contacto/Contacto.jsx
+++ src/containers/Contacto/Contacto.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
 import './contacto.css'
+import emailjs from '@emailjs/browser'
 
 const Contacto = () => {
+
+  
+  const sendMail = (event) => {
+    emailjs.sendForm('service_raly6as','template_c18fzit',event.target,'uo0UMj1yjJXx9ziY3')
+    .then (response => console.log(response))
+    .catch(error =>console.log(error))
+    alert('El formulario fue enviado correctamente, en breve nos vamos a poner en contacto con vos!')
+  }
+
   return (
     <>
       <div className='contactoContainer'>
@@ -12,28 +22,28 @@ const Contacto = () => {
               <img src="https://i.postimg.cc/26yw1C36/IMG-1185.jpg" alt="fotoContacto" />
             </div>
             <div>
-              <form className='formContainer'>
+              <form className='formContainer' onSubmit={sendMail}>
                 <div className='formJunto'>
                   <div className='formAbajo'>
                     <label> Nombre </label>
-                    <input type="text" />
+                    <input type="text" name='nombre' />
                   </div>
                   <div className='formAbajo'>
                     <label> Apellido </label>
-                    <input type="text" />
+                    <input type="text" name='apellido' />
                   </div>
                 </div>
                 <label> Email </label>
-                <input type="email" />
+                <input type="email" name='email' />
                 <label> Telefono de contacto </label>
-                <input type="tel" />
+                <input type="tel" name='telefono' />
                 <label>Motivo de contacto</label>
                 <select name="servicio" id="servicio">
-                  <option value="makeup">Pro Makeup</option>
-                  <option value="makeup">Bodas</option>
-                  <option value="makeup">15 Años</option>
-                  <option value="hairstyle">Pro Hairstyle</option>
-                  <option value="hayrstyle">Hair Botox Revitalize</option>
+                  <option value="Pro Make Up">Pro Makeup</option>
+                  <option value="Bodas">Bodas</option>
+                  <option value="15 años">15 Años</option>
+                  <option value="Pro Hairstyle">Pro Hairstyle</option>
+                  <option value="Hair Botox Revitalize">Hair Botox Revitalize</option>
                 </select>
                 <label> Mensaje</label>
                 <textarea name="mensaje" id="mensaje" cols="30" rows="6"></textarea>
