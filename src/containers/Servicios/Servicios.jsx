9ed2413c0107ab901056f1d5eec7ba6754705976
Agustin <90871968+Ler0v@users.noreply.github.com>
2022-12-23 00:14:56 -0300

    overflow y template de servicio

--- src/containers/Servicios/Servicios.jsx
+++ src/containers/Servicios/Servicios.jsx
@@ -69,6 +69,25 @@ const Servicios = () => {
             <button>Pro Hairstyle</button>
           </div>
           <div className='infoServicios'>
+            {info === 'Pro Makeup' && <div>
+              <h1>El servicio estrella de MUD</h1>
+              <p>Nos dedicamos hace mas de 13 años a resaltar la belleza natural de las personas a través del arte del maquillaje.
+              Nuestro objetivo es hacerte sentir bella, cómoda y natural. Utilizamos productos de primeras marcas, importados y nacionales, y las técnicas más novedosas y trend de la industria. Si tenes un evento en el cual queres lucirte, elegí maquillarte con profesionales capacitadas, elegi MUD.</p>
+              <h3>
+                En MUD te ofrecemos dos opciones:
+              </h3>
+              <ul>
+                <li>Maquillaje con Staff: contamos con un staff de artistas del maquillaje altamente capacitadas/os, quienes se perfeccionan constantemente y cuentan con las más novedosas técnicas y productos del mercado. Capacitadas por la directora de MUD. Maletín con productos variados de excelente calidad (importados, nacionales y MUD Cosmetics).</li>
+                <li>Maquillaje con Celeste Gonzales, directora de MUD: con más de 20 años de trayectoria, se capacito en las academias internacionales más prestigiosas del mundo, y cuenta con un maletin 100% importado, con marcas internacionales como Lancome, Make Up Forever, MAC, Dior, Givenchy, Chanel, Anastasia Beverly Hills, Natasha Denona, Kryolan, entre otras.</li>
+              </ul>
+              <h2>Ventajas</h2>
+              <ul>
+                <li>Utilizamos los mejores productos: invertimos en productos de la más altísima calidad para nuestro maletín, para obtener los mejores resultados</li>
+                <li>Máxima durabilidad: dejarte en manos de las profesionales MUD te garantizará un maquillaje intacto por hasta 10 horas. Conocemos los mejores trucos: sabemos implementar las técnicas más novedosas y que están en tendencia.</li>
+                <li>Optimizamos tu tiempo: al acudir a un gran evento, siempre hay muchos preparativos. Aunque sepas manejarte, puede que los nervios te jueguen una mala pasada y tengas que desmaquillarte y comenzar de cero. En MUD realizaremos todo el trabajo por vos, asegurando un resultado impecable.</li>
+                <li>Lucirás radiante en las fotos: hoy en día con la tecnología existente las cámaras que se utilizan en eventos no bajan de resoluciones como 4K, Ultra HD, y hasta 8K, donde hasta el más mínimo detalle se nota. Con un maquillaje profesional te aseguras de lucir impecable tanto en persona como a través de las cámaras.</li>
+              </ul>
+              </div>}  
             {info === '15 Años' && <div>
               <h1>hola</h1>
               <ul>
@@ -78,9 +97,6 @@ const Servicios = () => {
               </div>}
             {info === 'Bodas' && <div>
               <h2>Elegí tu Make-up ArtistEn MUD te ofrecemos dos opciones:Maquillaje con Staff: contamos con un staff de artistas del maquillaje altamente capacitadas/os, quienes se perfeccionan constantemente y cuentan con las más novedosas técnicas y productos del mercado.</h2>
-              </div>}  
-            {info === 'Pro Makeup' && <div>
-              <p>El servicio estrella de MUD. Nos dedicamos hace mas de 13 años a resaltar la belleza natural de las personas a través del arte del maquillaje. Nuestro objetivo es hacerte sentir bella, cómoda y natural. Utilizamos productos de primeras marcas, importados y nacionales, y las técnicas más novedosas y trend de la industria. Si tenes un evento en el cual queres lucirte, elegí maquillarte con profesionales capacitadas, elegi MUD.</p>
               </div>}  
           </div>
         </div>
