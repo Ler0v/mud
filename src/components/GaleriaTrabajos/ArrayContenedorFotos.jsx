09df178a82b8b20c3bae186650e0ce6368dba74f
Agustin <90871968+Ler0v@users.noreply.github.com>
2023-01-04 01:37:07 -0300

    array funcionando con slides

--- src/components/GaleriaTrabajos/ArrayContenedorFotos.jsx
+++ src/components/GaleriaTrabajos/ArrayContenedorFotos.jsx
@@ -22,3 +22,26 @@ const Imagen = {
 }
 
 export default Imagen;
+
+
+// [
+
+//     {img1: ('https://i.postimg.cc/B6nVgBym/1.jpg')},
+//     {img2: ('https://i.postimg.cc/TwmB4x4H/foto-academia-1.png')},
+//     {img3: ('https://i.postimg.cc/3NRjrTwZ/Whats-App-Image-2022-09-29-at-10-37-1.png')},
+//     {img4: ('https://i.postimg.cc/B6nVgBym/1.jpg')},
+//     {img5: ('https://i.postimg.cc/B6nVgBym/1.jpg')},
+//     {img6: ('https://i.postimg.cc/B6nVgBym/1.jpg')},
+//     {img7: ('https://i.postimg.cc/B6nVgBym/1.jpg')},
+//     {img8: ('https://i.postimg.cc/B6nVgBym/1.jpg')},
+//     {img9: ('https://i.postimg.cc/B6nVgBym/1.jpg')},
+//     {img10: ('https://i.postimg.cc/B6nVgBym/1.jpg')},
+//     {img11: ('https://i.postimg.cc/B6nVgBym/1.jpg')},
+//     {img12: ('https://i.postimg.cc/B6nVgBym/1.jpg')},
+//     {img13: ('https://i.postimg.cc/B6nVgBym/1.jpg')},
+//     {img14: ('https://i.postimg.cc/B6nVgBym/1.jpg')},
+//     {img16: ('https://i.postimg.cc/B6nVgBym/1.jpg')},
+//     {img17: ('https://i.postimg.cc/B6nVgBym/1.jpg')},
+//     {img18: ('https://i.postimg.cc/B6nVgBym/1.jpg')}
+
+// ]
--- src/components/GaleriaTrabajos/ArrayFotos.jsx
+++ src/components/GaleriaTrabajos/ArrayFotos.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import './arrayFotos.css'
 import Imagen from './ArrayContenedorFotos';
-
 import { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { 
@@ -18,7 +17,19 @@ const ArrayFotos = () => {
 
     const handleModal = () => {   
         setOpenModal(true)
-    }   
+    }
+
+
+    const handleCloseModal = () => {   
+        setOpenModal(false)
+    } 
+
+    const prevSlide = () => {   
+        setOpenModal(true)
+    }
+    const nextSlide = () => {   
+        setOpenModal(true)
+    }        
 
   return (
     <>
@@ -28,70 +39,72 @@ const ArrayFotos = () => {
                 <FontAwesomeIcon icon={faCircleXmark} className='btnClose'/>
                 <FontAwesomeIcon icon={faCircleChevronLeft} className='btnPrev'/>
                 <FontAwesomeIcon icon={faCircleChevronRight} className='btnNext'/>
-                <div className='fullScreenImage'></div>
+                <div className='fullScreenImage'>
+                    <img src={Imagen} alt="imgFullscreen"  />
+                </div>
             </div>
         }
         
         <h1 className='titulo'>Nuestros Trabajos</h1>
         <div className="slider" >
 
-            <div className="slider-track" onClick={ ()=> handleModal()}>
+            <div className="slider-track">
                 
                 <div className='slide' onClick={ ()=> handleModal()}>
                     <img src={Imagen.img1} className='imgGaleria' alt="g1" />
                 </div>
-                <div className='slide'>
+                <div className='slide' onClick={ ()=> handleModal()}>
                     <img src={Imagen.img2} className='imgGaleria' alt="g2" />
                 </div>
-                <div className='slide'>
+                <div className='slide' onClick={ ()=> handleModal()}>
                     <img src={Imagen.img3} className='imgGaleria' alt="g3" />
                 </div>
-                <div className='slide'>
+                <div className='slide' onClick={ ()=> handleModal()}>
                     <img src={Imagen.img1} className='imgGaleria' alt="g4" />
                 </div>
-                <div className='slide'>
+                <div className='slide' onClick={ ()=> handleModal()}>
                     <img src={Imagen.img1} className='imgGaleria' alt="g5" />
                 </div>
-                <div className='slide'>
+                <div className='slide' onClick={ ()=> handleModal()}>
                     <img src={Imagen.img1} className='imgGaleria' alt="g6" />
                 </div>
-                <div className='slide'>
+                <div className='slide' onClick={ ()=> handleModal()}>
                     <img src={Imagen.img1} className='imgGaleria' alt="g7" />
                 </div>
-                <div className='slide'>
+                <div className='slide' onClick={ ()=> handleModal()}>
                     <img src={Imagen.img1} className='imgGaleria' alt="g8" />
                 </div>
-                <div className='slide'>
+                <div className='slide' onClick={ ()=> handleModal()}>
                     <img src={Imagen.img1} className='imgGaleria' alt="g9" />
                 </div>
 
                 {/* /* slide 2 */}
 
-                <div className='slide'>
+                <div className='slide' onClick={ ()=> handleModal()}>
                     <img src={Imagen.img1} className='imgGaleria' alt="g10" />
                 </div>
-                <div className='slide'>
+                <div className='slide' onClick={ ()=> handleModal()}>
                     <img src={Imagen.img2} className='imgGaleria' alt="g11" />
                 </div>
-                <div className='slide'>
+                <div className='slide' onClick={ ()=> handleModal()}>
                     <img src={Imagen.img3} className='imgGaleria' alt="g12" />
                 </div>
-                <div className='slide'>
+                <div className='slide' onClick={ ()=> handleModal()}>
                     <img src={Imagen.img1} className='imgGaleria' alt="g13" />
                 </div>
-                <div className='slide'>
+                <div className='slide' onClick={ ()=> handleModal()}>
                     <img src={Imagen.img1} className='imgGaleria' alt="g14" />
                 </div>
-                <div className='slide'>
+                <div className='slide' onClick={ ()=> handleModal()}>
                     <img src={Imagen.img1} className='imgGaleria' alt="g15" />
                 </div>
-                <div className='slide'>
+                <div className='slide' onClick={ ()=> handleModal()}>
                     <img src={Imagen.img1} className='imgGaleria' alt="g16" />
                 </div>
-                <div className='slide'>
+                <div className='slide' onClick={ ()=> handleModal()}>
                     <img src={Imagen.img1} className='imgGaleria' alt="g17" />
                 </div>
-                <div className='slide'>
+                <div className='slide' onClick={ ()=> handleModal()}>
                     <img src={Imagen.img1} className='imgGaleria' alt="g18" />
                 </div>
 
