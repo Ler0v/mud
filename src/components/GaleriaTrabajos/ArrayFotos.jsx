44e8ee1fbb5fd56f7eddf31119ab0e8178f4a77f
Agustin <90871968+Ler0v@users.noreply.github.com>
2023-01-03 23:39:05 -0300

    import de imagenes trabajos

--- src/components/GaleriaTrabajos/ArrayFotos.jsx
+++ src/components/GaleriaTrabajos/ArrayFotos.jsx
@@ -1,77 +1,98 @@
 import React from 'react'
 import './arrayFotos.css'
+import Imagen from './ArrayContenedorFotos';
 
-const ArrayFotos = (img) => {
+import { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { 
+            faCircleChevronLeft,
+            faCircleChevronRight,
+            faCircleXmark
+        } 
+from '@fortawesome/free-solid-svg-icons';
 
-    // const Galeria = [
-    //     {
-    //         img: 'https://i.postimg.cc/26yw1C36/IMG-1185.jpg'
-    //     }
-    // ]
+
+const ArrayFotos = () => {
+
+    const [openModal, setOpenModal] = useState(false);
+
+    const handleModal = () => {   
+        setOpenModal(true)
+    }   
 
   return (
     <>
 
-        <div className="slider">
+        {openModal &&
+            <div className='sliderWrap'>
+                <FontAwesomeIcon icon={faCircleXmark} className='btnClose'/>
+                <FontAwesomeIcon icon={faCircleChevronLeft} className='btnPrev'/>
+                <FontAwesomeIcon icon={faCircleChevronRight} className='btnNext'/>
+                <div className='fullScreenImage'></div>
+            </div>
+        }
+        
+        <h1 className='titulo'>Nuestros Trabajos</h1>
+        <div className="slider" >
 
-            <div className="slider-track">
+            <div className="slider-track" onClick={ ()=> handleModal()}>
                 
-                <div className='slide'>
-                    <img src='https://i.postimg.cc/B6nVgBym/1.jpg' className='imgGaleria' alt="g1" />
+                <div className='slide' onClick={ ()=> handleModal()}>
+                    <img src={Imagen.img1} className='imgGaleria' alt="g1" />
                 </div>
                 <div className='slide'>
-                    <img src='https://i.postimg.cc/B6nVgBym/1.jpg' className='imgGaleria' alt="g2" />
+                    <img src={Imagen.img2} className='imgGaleria' alt="g2" />
                 </div>
                 <div className='slide'>
-                    <img src='https://i.postimg.cc/B6nVgBym/1.jpg' className='imgGaleria' alt="g3" />
+                    <img src={Imagen.img3} className='imgGaleria' alt="g3" />
                 </div>
                 <div className='slide'>
-                    <img src='https://i.postimg.cc/B6nVgBym/1.jpg' className='imgGaleria' alt="g4" />
+                    <img src={Imagen.img1} className='imgGaleria' alt="g4" />
                 </div>
                 <div className='slide'>
-                    <img src='https://i.postimg.cc/B6nVgBym/1.jpg' className='imgGaleria' alt="g5" />
+                    <img src={Imagen.img1} className='imgGaleria' alt="g5" />
                 </div>
                 <div className='slide'>
-                    <img src='https://i.postimg.cc/B6nVgBym/1.jpg' className='imgGaleria' alt="g6" />
+                    <img src={Imagen.img1} className='imgGaleria' alt="g6" />
                 </div>
                 <div className='slide'>
-                    <img src='https://i.postimg.cc/B6nVgBym/1.jpg' className='imgGaleria' alt="g7" />
+                    <img src={Imagen.img1} className='imgGaleria' alt="g7" />
                 </div>
                 <div className='slide'>
-                    <img src='https://i.postimg.cc/B6nVgBym/1.jpg' className='imgGaleria' alt="g8" />
+                    <img src={Imagen.img1} className='imgGaleria' alt="g8" />
                 </div>
                 <div className='slide'>
-                    <img src='https://i.postimg.cc/B6nVgBym/1.jpg' className='imgGaleria' alt="g9" />
+                    <img src={Imagen.img1} className='imgGaleria' alt="g9" />
                 </div>
 
-                {/* slide 2 */}
+                {/* /* slide 2 */}
 
                 <div className='slide'>
-                    <img src='https://i.postimg.cc/B6nVgBym/1.jpg' className='imgGaleria' alt="g10" />
+                    <img src={Imagen.img1} className='imgGaleria' alt="g10" />
                 </div>
                 <div className='slide'>
-                    <img src='https://i.postimg.cc/B6nVgBym/1.jpg' className='imgGaleria' alt="g11" />
+                    <img src={Imagen.img2} className='imgGaleria' alt="g11" />
                 </div>
                 <div className='slide'>
-                    <img src='https://i.postimg.cc/B6nVgBym/1.jpg' className='imgGaleria' alt="g12" />
+                    <img src={Imagen.img3} className='imgGaleria' alt="g12" />
                 </div>
                 <div className='slide'>
-                    <img src='https://i.postimg.cc/B6nVgBym/1.jpg' className='imgGaleria' alt="g13" />
+                    <img src={Imagen.img1} className='imgGaleria' alt="g13" />
                 </div>
                 <div className='slide'>
-                    <img src='https://i.postimg.cc/B6nVgBym/1.jpg' className='imgGaleria' alt="g14" />
+                    <img src={Imagen.img1} className='imgGaleria' alt="g14" />
                 </div>
                 <div className='slide'>
-                    <img src='https://i.postimg.cc/B6nVgBym/1.jpg' className='imgGaleria' alt="g15" />
+                    <img src={Imagen.img1} className='imgGaleria' alt="g15" />
                 </div>
                 <div className='slide'>
-                    <img src='https://i.postimg.cc/B6nVgBym/1.jpg' className='imgGaleria' alt="g16" />
+                    <img src={Imagen.img1} className='imgGaleria' alt="g16" />
                 </div>
                 <div className='slide'>
-                    <img src='https://i.postimg.cc/B6nVgBym/1.jpg' className='imgGaleria' alt="g17" />
+                    <img src={Imagen.img1} className='imgGaleria' alt="g17" />
                 </div>
                 <div className='slide'>
-                    <img src='https://i.postimg.cc/B6nVgBym/1.jpg' className='imgGaleria' alt="g18" />
+                    <img src={Imagen.img1} className='imgGaleria' alt="g18" />
                 </div>
 
             </div>
