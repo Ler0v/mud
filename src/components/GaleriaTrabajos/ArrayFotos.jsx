bbe981df0a34a45ece5735fd6b81d37dd394ad17
Agustin <90871968+Ler0v@users.noreply.github.com>
2023-01-05 21:34:34 -0300

    correccion fotos servicios

--- src/components/GaleriaTrabajos/ArrayFotos.jsx
+++ src/components/GaleriaTrabajos/ArrayFotos.jsx
@@ -50,61 +50,61 @@ const ArrayFotos = () => {
 
             <div className="slider-track">
                 
-                <div className='slide' onClick={ ()=> handleModal()}>
+                <div className='fotosMovimiento' onClick={ ()=> handleModal()}>
                     <img src={Imagen.img1} className='imgGaleria' alt="g1" />
                 </div>
-                <div className='slide' onClick={ ()=> handleModal()}>
+                <div className='fotosMovimiento' onClick={ ()=> handleModal()}>
                     <img src={Imagen.img2} className='imgGaleria' alt="g2" />
                 </div>
-                <div className='slide' onClick={ ()=> handleModal()}>
+                <div className='fotosMovimiento' onClick={ ()=> handleModal()}>
                     <img src={Imagen.img3} className='imgGaleria' alt="g3" />
                 </div>
-                <div className='slide' onClick={ ()=> handleModal()}>
+                <div className='fotosMovimiento' onClick={ ()=> handleModal()}>
                     <img src={Imagen.img1} className='imgGaleria' alt="g4" />
                 </div>
-                <div className='slide' onClick={ ()=> handleModal()}>
+                <div className='fotosMovimiento' onClick={ ()=> handleModal()}>
                     <img src={Imagen.img1} className='imgGaleria' alt="g5" />
                 </div>
-                <div className='slide' onClick={ ()=> handleModal()}>
+                <div className='fotosMovimiento' onClick={ ()=> handleModal()}>
                     <img src={Imagen.img1} className='imgGaleria' alt="g6" />
                 </div>
-                <div className='slide' onClick={ ()=> handleModal()}>
+                <div className='fotosMovimiento' onClick={ ()=> handleModal()}>
                     <img src={Imagen.img1} className='imgGaleria' alt="g7" />
                 </div>
-                <div className='slide' onClick={ ()=> handleModal()}>
+                <div className='fotosMovimiento' onClick={ ()=> handleModal()}>
                     <img src={Imagen.img1} className='imgGaleria' alt="g8" />
                 </div>
-                <div className='slide' onClick={ ()=> handleModal()}>
+                <div className='fotosMovimiento' onClick={ ()=> handleModal()}>
                     <img src={Imagen.img1} className='imgGaleria' alt="g9" />
                 </div>
 
-                {/* /* slide 2 */}
+                {/* /* fotosMovimiento 2 */}
 
-                <div className='slide' onClick={ ()=> handleModal()}>
+                <div className='fotosMovimiento' onClick={ ()=> handleModal()}>
                     <img src={Imagen.img1} className='imgGaleria' alt="g10" />
                 </div>
-                <div className='slide' onClick={ ()=> handleModal()}>
+                <div className='fotosMovimiento' onClick={ ()=> handleModal()}>
                     <img src={Imagen.img2} className='imgGaleria' alt="g11" />
                 </div>
-                <div className='slide' onClick={ ()=> handleModal()}>
+                <div className='fotosMovimiento' onClick={ ()=> handleModal()}>
                     <img src={Imagen.img3} className='imgGaleria' alt="g12" />
                 </div>
-                <div className='slide' onClick={ ()=> handleModal()}>
+                <div className='fotosMovimiento' onClick={ ()=> handleModal()}>
                     <img src={Imagen.img1} className='imgGaleria' alt="g13" />
                 </div>
-                <div className='slide' onClick={ ()=> handleModal()}>
+                <div className='fotosMovimiento' onClick={ ()=> handleModal()}>
                     <img src={Imagen.img1} className='imgGaleria' alt="g14" />
                 </div>
-                <div className='slide' onClick={ ()=> handleModal()}>
+                <div className='fotosMovimiento' onClick={ ()=> handleModal()}>
                     <img src={Imagen.img1} className='imgGaleria' alt="g15" />
                 </div>
-                <div className='slide' onClick={ ()=> handleModal()}>
+                <div className='fotosMovimiento' onClick={ ()=> handleModal()}>
                     <img src={Imagen.img1} className='imgGaleria' alt="g16" />
                 </div>
-                <div className='slide' onClick={ ()=> handleModal()}>
+                <div className='fotosMovimiento' onClick={ ()=> handleModal()}>
                     <img src={Imagen.img1} className='imgGaleria' alt="g17" />
                 </div>
-                <div className='slide' onClick={ ()=> handleModal()}>
+                <div className='fotosMovimiento' onClick={ ()=> handleModal()}>
                     <img src={Imagen.img1} className='imgGaleria' alt="g18" />
                 </div>
 
