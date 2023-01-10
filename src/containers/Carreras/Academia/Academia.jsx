54e16551eea415048c45a65597cfe829677898c4
Agustin <90871968+Ler0v@users.noreply.github.com>
2023-01-10 17:44:34 -0300

    template servicio en academia

--- src/containers/Carreras/Academia/Academia.jsx
+++ src/containers/Carreras/Academia/Academia.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './academia.css'
-import InfoAcademia from './infoAcademia/InfoAcademia'
+import { useState } from 'react'
 
 
 
@@ -20,59 +20,26 @@ const Academia = () => {
               <img src="https://i.postimg.cc/vmgHLtJZ/SISTEMA-DE-EDUCACION-MUD-ACADEMY-FULL-BLANCO-1.png" alt="headerimg2" />
             </div>
           </div>
-          <div className='academiaItems'>
-            <div className='MUDMaquillajeProfesional'>
-              <h1>Nuestras Carreras</h1>
-              <div className='infoAcademia'>
-                <img src="https://i.postimg.cc/3NRjrTwZ/Whats-App-Image-2022-09-29-at-10-37-1.png" alt="fotoAcademia" />
-                <div className='seccionAcademia'>
-                  <button><a href='#carreraMaquillaje'>Carrera de maquillaje profesional</a></button>
-                  <button><a href='#cursoMaquillaje'>Curso de maquillaje social</a></button>
-                  <button><a href='#tallerCejas'>Diseño y perfilado de cejas + Henna</a></button>
-                  <button><a href='#tallerCejasHenna'>Brow Lamination</a></button>
-                  <button><a href='#tallerLifting'>Lash Lifting</a></button>
-                  <button><a href='#cursoPeinados'>Curso de peinados</a></button>
-                  <button><a href='#tallertrenzas'>Taller de trenzas</a></button>
+          <div className='academia'>
+            <div className="academiaCarousel">
+              <img src="https://i.postimg.cc/3NRjrTwZ/Whats-App-Image-2022-09-29-at-10-37-1.png" alt="fotoAcademia" />
+            </div>
+            <div className='academiaInfo'>
+              <h1>Academia</h1>
+              <div className='academiaBotones'>
+                  <button>Carrera de maquillaje profesional</button>
+                  <button>Curso de maquillaje socia</button>
+                  <button>Diseño y perfilado de cejas + Henna</button>
+                  <button>Brow Lamination</button>
+                  <button>Lash Lifting</button>
+                  <button>Curso de peinados</button>
+                  <button>Taller de trenzas</button>
                 </div>
-              </div>
+                <div className="academiaInfo">
+                
+                </div>
             </div>
           </div>
-            <section className='academiaInfo' id='carreraMaquillaje'>
-              <InfoAcademia imagen={'https://i.postimg.cc/B6nVgBym/1.jpg'} titulo={'Carrera de maquillaje profesional'} info1={'Ideal para apasionados por el maquillaje y trabajo creativo'} info2={'Rapida y rentable salida laboral'} info3={'Pasantias profesionales en producciones y desfiles'} info4={'Credencial profesional'} info5={'Certificado profesional'} info6={'Portafolio profesional'} info7={'Duracion 6 meses y clases de 3 horas'}
-              info21={'La carrera de maquillaje profesional tiene una duracion de 6 meses , se cursa 1 vez por por semana en 3 clases por hora'} info22={'Tiene una dinámica teórico/práctica/audiovisual ágil para poder avanzar en la formación. Está organizada por niveles para la correcta formación de los profesionales'} banner1={'Beauty'} banner2={'Fashion'} banner3={'Artistic'}
-              bannerInfo={'-Preparacion de la piel y primeros pasos.'} bannerInfo2={'-Todo sobre ojos: anatomia, formologia, sobreados, contraste, esfumados, delineados, cejas, pestanias.'} bannerInfo3={'-Boca, correciones ovalo facial, rubor, highlight.'} bannerInfo4={'-Sociales: novias, 15 anios, madrinas, eventos.'} bannerInfo5={'-Smoke eyes'} bannerInfo21={'-Maquillaje con texturas, patinas, efectos, pigmentos.'} bannerInfo22={'-Cut crease.'} bannerInfo23={'-Epoca'} bannerInfo24={'-Disenio de cejas'} bannerInfo25={'-Pasarella'} bannerInfo26={'-Face Chart'} bannerInfo27={'-Fotografia'} bannerInfo28={'-Editorial'} bannerInfo31={'-Cine HD'} bannerInfo32={'-TV HD'} bannerInfo33={'-Efectos para cine'} bannerInfo34={'-Caracterizaciones'} bannerInfo35={'-Artistico'} bannerInfo36={'-Teatro'}
-              />
-            </section>
-            <div className='academiaInfo' id='cursoMaquillaje'>
-              <InfoAcademia  imagen={'https://i.postimg.cc/Kv0vyGFs/IMG-1265.jpg'} titulo={'Curso de maquillaje social'} info1={'Ideal para apasionados por el maquillaje y trabajo creativo'} info2={'Rapida y rentable salida laboral'} info3={'Pasantias profesionales en producciones y desfiles'} info4={'Credencial profesional'} info5={'Certificado profesional'} info6={'Portafolio profesional'} info7={'Duracion 6 meses y clases de 3 horas'}
-              info21={'La carrera de maquillaje profesional tiene una duracion de 6 meses , se cursa 1 vez por por semana en 3 clases por hora'} info22={'Tiene una dinámica teórico/práctica/audiovisual ágil para poder avanzar en la formación. Está organizada por niveles para la correcta formación de los profesionales'} banner1={'Beauty'} banner2={'Fashion'} banner3={'Artistic'}
-              bannerInfo={'-Preparacion de la piel y primeros pasos.'} bannerInfo2={'-Todo sobre ojos: anatomia, formologia, sobreados, contraste, esfumados, delineados, cejas, pestanias.'} bannerInfo3={'-Boca, correciones ovalo facial, rubor, highlight.'} bannerInfo4={'-Sociales: novias, 15 anios, madrinas, eventos.'} bannerInfo5={'-Smoke eyes'} bannerInfo21={'-Maquillaje con texturas, patinas, efectos, pigmentos.'} bannerInfo22={'-Cut crease.'} bannerInfo23={'-Epoca'} bannerInfo24={'-Disenio de cejas'} bannerInfo25={'-Pasarella'} bannerInfo26={'-Face Chart'} bannerInfo27={'-Fotografia'} bannerInfo28={'-Editorial'} bannerInfo31={'-Cine HD'} bannerInfo32={'-TV HD'} bannerInfo33={'-Efectos para cine'} bannerInfo34={'-Caracterizaciones'} bannerInfo35={'-Artistico'} bannerInfo36={'-Teatro'} />
-            </div>
-            <div className='academiaInfo' id='tallerCejas' >
-              <InfoAcademia imagen={'https://i.postimg.cc/Jh7Ytzqd/7.jpg'} titulo={'Taller de laminado de cejas'} info1={'Ideal para apasionados por el maquillaje y trabajo creativo'} info2={'Rapida y rentable salida laboral'} info3={'Pasantias profesionales en producciones y desfiles'} info4={'Credencial profesional'} info5={'Certificado profesional'} info6={'Portafolio profesional'} info7={'Duracion 6 meses y clases de 3 horas'}
-              info21={'La carrera de maquillaje profesional tiene una duracion de 6 meses , se cursa 1 vez por por semana en 3 clases por hora'} info22={'Tiene una dinámica teórico/práctica/audiovisual ágil para poder avanzar en la formación. Está organizada por niveles para la correcta formación de los profesionales'} banner1={'Beauty'} banner2={'Fashion'} banner3={'Artistic'}
-              bannerInfo={'-Preparacion de la piel y primeros pasos.'} bannerInfo2={'-Todo sobre ojos: anatomia, formologia, sobreados, contraste, esfumados, delineados, cejas, pestanias.'} bannerInfo3={'-Boca, correciones ovalo facial, rubor, highlight.'} bannerInfo4={'-Sociales: novias, 15 anios, madrinas, eventos.'} bannerInfo5={'-Smoke eyes'} bannerInfo21={'-Maquillaje con texturas, patinas, efectos, pigmentos.'} bannerInfo22={'-Cut crease.'} bannerInfo23={'-Epoca'} bannerInfo24={'-Disenio de cejas'} bannerInfo25={'-Pasarella'} bannerInfo26={'-Face Chart'} bannerInfo27={'-Fotografia'} bannerInfo28={'-Editorial'} bannerInfo31={'-Cine HD'} bannerInfo32={'-TV HD'} bannerInfo33={'-Efectos para cine'} bannerInfo34={'-Caracterizaciones'} bannerInfo35={'-Artistico'} bannerInfo36={'-Teatro'} />
-            </div>
-            <div className='academiaInfo' id='tallerLifting'>
-              <InfoAcademia  imagen={'https://i.postimg.cc/B6nVgBym/1.jpg'} titulo={'Taller de lifting + nutricion + tinte'} info1={'Ideal para apasionados por el maquillaje y trabajo creativo'} info2={'Rapida y rentable salida laboral'} info3={'Pasantias profesionales en producciones y desfiles'} info4={'Credencial profesional'} info5={'Certificado profesional'} info6={'Portafolio profesional'} info7={'Duracion 6 meses y clases de 3 horas'}
-              info21={'La carrera de maquillaje profesional tiene una duracion de 6 meses , se cursa 1 vez por por semana en 3 clases por hora'} info22={'Tiene una dinámica teórico/práctica/audiovisual ágil para poder avanzar en la formación. Está organizada por niveles para la correcta formación de los profesionales'} banner1={'Beauty'} banner2={'Fashion'} banner3={'Artistic'}
-              bannerInfo={'-Preparacion de la piel y primeros pasos.'} bannerInfo2={'-Todo sobre ojos: anatomia, formologia, sobreados, contraste, esfumados, delineados, cejas, pestanias.'} bannerInfo3={'-Boca, correciones ovalo facial, rubor, highlight.'} bannerInfo4={'-Sociales: novias, 15 anios, madrinas, eventos.'} bannerInfo5={'-Smoke eyes'} bannerInfo21={'-Maquillaje con texturas, patinas, efectos, pigmentos.'} bannerInfo22={'-Cut crease.'} bannerInfo23={'-Epoca'} bannerInfo24={'-Disenio de cejas'} bannerInfo25={'-Pasarella'} bannerInfo26={'-Face Chart'} bannerInfo27={'-Fotografia'} bannerInfo28={'-Editorial'} bannerInfo31={'-Cine HD'} bannerInfo32={'-TV HD'} bannerInfo33={'-Efectos para cine'} bannerInfo34={'-Caracterizaciones'} bannerInfo35={'-Artistico'} bannerInfo36={'-Teatro'} />
-            </div>
-            <div className='academiaInfo' id='tallerCejasHenna'>
-              <InfoAcademia  imagen={'https://i.postimg.cc/B6nVgBym/1.jpg'} titulo={'Taller de disenio de cejas + henna'} info1={'Ideal para apasionados por el maquillaje y trabajo creativo'} info2={'Rapida y rentable salida laboral'} info3={'Pasantias profesionales en producciones y desfiles'} info4={'Credencial profesional'} info5={'Certificado profesional'} info6={'Portafolio profesional'} info7={'Duracion 6 meses y clases de 3 horas'}
-              info21={'La carrera de maquillaje profesional tiene una duracion de 6 meses , se cursa 1 vez por por semana en 3 clases por hora'} info22={'Tiene una dinámica teórico/práctica/audiovisual ágil para poder avanzar en la formación. Está organizada por niveles para la correcta formación de los profesionales'} banner1={'Beauty'} banner2={'Fashion'} banner3={'Artistic'}
-              bannerInfo={'-Preparacion de la piel y primeros pasos.'} bannerInfo2={'-Todo sobre ojos: anatomia, formologia, sobreados, contraste, esfumados, delineados, cejas, pestanias.'} bannerInfo3={'-Boca, correciones ovalo facial, rubor, highlight.'} bannerInfo4={'-Sociales: novias, 15 anios, madrinas, eventos.'} bannerInfo5={'-Smoke eyes'} bannerInfo21={'-Maquillaje con texturas, patinas, efectos, pigmentos.'} bannerInfo22={'-Cut crease.'} bannerInfo23={'-Epoca'} bannerInfo24={'-Disenio de cejas'} bannerInfo25={'-Pasarella'} bannerInfo26={'-Face Chart'} bannerInfo27={'-Fotografia'} bannerInfo28={'-Editorial'} bannerInfo31={'-Cine HD'} bannerInfo32={'-TV HD'} bannerInfo33={'-Efectos para cine'} bannerInfo34={'-Caracterizaciones'} bannerInfo35={'-Artistico'} bannerInfo36={'-Teatro'} />
-            </div>
-            <div className='academiaInfo' id='cursoPeinados'>
-              <InfoAcademia  imagen={'https://i.postimg.cc/B6nVgBym/1.jpg'} titulo={'Curso de peinados'} info1={'Ideal para apasionados por el maquillaje y trabajo creativo'} info2={'Rapida y rentable salida laboral'} info3={'Pasantias profesionales en producciones y desfiles'} info4={'Credencial profesional'} info5={'Certificado profesional'} info6={'Portafolio profesional'} info7={'Duracion 6 meses y clases de 3 horas'}
-              info21={'La carrera de maquillaje profesional tiene una duracion de 6 meses , se cursa 1 vez por por semana en 3 clases por hora'} info22={'Tiene una dinámica teórico/práctica/audiovisual ágil para poder avanzar en la formación. Está organizada por niveles para la correcta formación de los profesionales'} banner1={'Beauty'} banner2={'Fashion'} banner3={'Artistic'}
-              bannerInfo={'-Preparacion de la piel y primeros pasos.'} bannerInfo2={'-Todo sobre ojos: anatomia, formologia, sobreados, contraste, esfumados, delineados, cejas, pestanias.'} bannerInfo3={'-Boca, correciones ovalo facial, rubor, highlight.'} bannerInfo4={'-Sociales: novias, 15 anios, madrinas, eventos.'} bannerInfo5={'-Smoke eyes'} bannerInfo21={'-Maquillaje con texturas, patinas, efectos, pigmentos.'} bannerInfo22={'-Cut crease.'} bannerInfo23={'-Epoca'} bannerInfo24={'-Disenio de cejas'} bannerInfo25={'-Pasarella'} bannerInfo26={'-Face Chart'} bannerInfo27={'-Fotografia'} bannerInfo28={'-Editorial'} bannerInfo31={'-Cine HD'} bannerInfo32={'-TV HD'} bannerInfo33={'-Efectos para cine'} bannerInfo34={'-Caracterizaciones'} bannerInfo35={'-Artistico'} bannerInfo36={'-Teatro'} />
-            </div>
-            <div className='academiaInfo' id='tallertrenzas'>
-              <InfoAcademia  imagen={'https://i.postimg.cc/C1pw4dHd/IMG-3807.jpg'} titulo={'Carrera de maquillaje profesional'} info1={'Ideal para apasionados por el maquillaje y trabajo creativo'} info2={'Rapida y rentable salida laboral'} info3={'Pasantias profesionales en producciones y desfiles'} info4={'Credencial profesional'} info5={'Certificado profesional'} info6={'Portafolio profesional'} info7={'Duracion 6 meses y clases de 3 horas'}
-              info21={'La carrera de maquillaje profesional tiene una duracion de 6 meses , se cursa 1 vez por por semana en 3 clases por hora'} info22={'Tiene una dinámica teórico/práctica/audiovisual ágil para poder avanzar en la formación. Está organizada por niveles para la correcta formación de los profesionales'} banner1={'Beauty'} banner2={'Fashion'} banner3={'Artistic'}
-              bannerInfo={'-Preparacion de la piel y primeros pasos.'} bannerInfo2={'-Todo sobre ojos: anatomia, formologia, sobreados, contraste, esfumados, delineados, cejas, pestanias.'} bannerInfo3={'-Boca, correciones ovalo facial, rubor, highlight.'} bannerInfo4={'-Sociales: novias, 15 anios, madrinas, eventos.'} bannerInfo5={'-Smoke eyes'} bannerInfo21={'-Maquillaje con texturas, patinas, efectos, pigmentos.'} bannerInfo22={'-Cut crease.'} bannerInfo23={'-Epoca'} bannerInfo24={'-Disenio de cejas'} bannerInfo25={'-Pasarella'} bannerInfo26={'-Face Chart'} bannerInfo27={'-Fotografia'} bannerInfo28={'-Editorial'} bannerInfo31={'-Cine HD'} bannerInfo32={'-TV HD'} bannerInfo33={'-Efectos para cine'} bannerInfo34={'-Caracterizaciones'} bannerInfo35={'-Artistico'} bannerInfo36={'-Teatro'} />
-            </div>
         </div>
     </>
   )
