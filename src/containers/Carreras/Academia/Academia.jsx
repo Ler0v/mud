b50c0cea301a775cd6255c3ab77dc4f094d58985
Agustin <90871968+Ler0v@users.noreply.github.com>
2023-01-29 20:13:18 -0300

    academia fix

--- src/containers/Carreras/Academia/Academia.jsx
+++ src/containers/Carreras/Academia/Academia.jsx
@@ -15,8 +15,8 @@ const Academia = () => {
           <div className='headerAcademia'>
             <div className='headerItemPosition1'>
               <p className='headerItem1'>MUD Academy</p>
-              <p className='headerItem2'>Aprende las mejores técnicas de la mano de artistas certificados, aspira al trabajo de tus sueños</p>
               <p className='headerItem3'>CONVERTITE EN UN MAKEUP ARTIST</p>
+              <p className='headerItem2'>Aprende las mejores técnicas de la mano de artistas certificados, aspira al trabajo de tus sueños</p>
             </div>
             <div className='headerItemPosition2'>
               <img src="https://i.postimg.cc/vmgHLtJZ/SISTEMA-DE-EDUCACION-MUD-ACADEMY-FULL-BLANCO-1.png" alt="headerimg2" />
