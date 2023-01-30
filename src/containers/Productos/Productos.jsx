5b61b243d1d7a005ffa48fa2c949810e600fbb90
Agustin <90871968+Ler0v@users.noreply.github.com>
2023-01-29 21:16:23 -0300

    productos prox responsive

--- src/containers/Productos/Productos.jsx
+++ src/containers/Productos/Productos.jsx
@@ -7,7 +7,7 @@ const Productos = () => {
     <div className='seccionProductos'>
         <h1 className='titulo'>Nuestra tienda esta en proceso!</h1>
         <p>Por ahora nuestros productos estan disponibles en el siguiente enlace:</p>
-        <a href="https://mudmakeupdesign.mitiendanube.com/productos/">Tienda</a>
+        <a href="https://mudmakeupdesign.mitiendanube.com/productos/" rel='noopener' target="_blank">Tienda</a>
     </div>
 </>
   )
