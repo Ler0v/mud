085924f9721080454fceb273999aea84c046959f
Agustin <90871968+Ler0v@users.noreply.github.com>
2023-04-20 22:24:26 -0300

    fotos incluidas en array

--- src/components/GaleriaTrabajos/ArrayContenedorFotos.jsx
+++ src/components/GaleriaTrabajos/ArrayContenedorFotos.jsx
@@ -6,11 +6,11 @@ const Imagen = [
     },
     {
         id: 2,
-        img: 'https://i.postimg.cc/nz2R47hL/423A8780.jpg'
+        img: 'https://i.postimg.cc/d3X969PJ/IMG-20210216-WA0015.jpg'
     },
     {
         id: 3,
-        img: 'https://i.postimg.cc/V6Kgr8Kr/423A8901.jpg'
+        img: 'https://i.postimg.cc/j2CXyF18/IMG-20210216-WA0011.jpg'
     },
     {
         id: 4,
@@ -18,19 +18,19 @@ const Imagen = [
     },
     {
         id: 5,
-        img: 'https://i.postimg.cc/j2CXyF18/IMG-20210216-WA0011.jpg'
+        img: 'https://i.postimg.cc/PJFScjZs/B0014199-b.jpg'
     },
     {
         id: 6,
-        img: 'https://i.postimg.cc/d3X969PJ/IMG-20210216-WA0015.jpg'
+        img: 'https://i.postimg.cc/nz2R47hL/423A8780.jpg'
     },
     {
         id: 7,
-        img: 'https://i.postimg.cc/SsSVxsfb/IMG-20210216-WA0003.jpg'
+        img: 'https://i.postimg.cc/V6Kgr8Kr/423A8901.jpg'
     },
     {
         id: 8,
-        img: 'https://i.postimg.cc/sf0K33Bb/IMG-20210323-WA0032.jpg'
+        img: 'https://i.postimg.cc/SsSVxsfb/IMG-20210216-WA0003.jpg'
     },
     {
         id: 9,
@@ -38,65 +38,25 @@ const Imagen = [
     },
     {
         id: 10,
-        img: 'https://i.postimg.cc/PJFScjZs/B0014199-b.jpg'
+        img: 'https://i.postimg.cc/7LN02xX3/423A9270.jpg'
     },
     {
         id: 11,
-        img: 'https://i.postimg.cc/3NRjrTwZ/Whats-App-Image-2022-09-29-at-10-37-1.png'
+        img: 'https://i.postimg.cc/sf0K33Bb/IMG-20210323-WA0032.jpg'
     },
     {
         id: 12,
-        img: 'https://i.postimg.cc/TwmB4x4H/foto-academia-1.png'
+        img: 'https://i.postimg.cc/qvy8pc3Z/IMG-20210323-WA0040.jpg'
     },
     {
         id: 13,
-        img: 'https://i.postimg.cc/B6nVgBym/1.jpg'
+        img: 'https://i.postimg.cc/L6rtdQn1/IMG-20221203-203433-628.jpg'
     },
     {
         id: 14,
-        img: 'https://i.postimg.cc/B6nVgBym/1.jpg'
-    },
-    {
-        id: 15,
-        img: 'https://i.postimg.cc/B6nVgBym/1.jpg'
-    },
-    {
-        id: 16,
-        img: 'https://i.postimg.cc/B6nVgBym/1.jpg'
-    },
-    {
-        id: 17,
-        img: 'https://i.postimg.cc/B6nVgBym/1.jpg'
-    },
-    {
-        id: 18,
-        img: 'https://i.postimg.cc/B6nVgBym/1.jpg'
-    },
+        img: 'https://i.postimg.cc/pdvKr9QV/IMG-20221203-203425-442.jpg'
+    }
 
 ]
 
 export default Imagen;
-
-
-// {
-
-//     img1:('https://i.postimg.cc/B6nVgBym/1.jpg'),
-//     img2: ('https://i.postimg.cc/TwmB4x4H/foto-academia-1.png'),  
-//     img3: ('https://i.postimg.cc/3NRjrTwZ/Whats-App-Image-2022-09-29-at-10-37-1.png'),
-//     img4: ('https://i.postimg.cc/B6nVgBym/1.jpg'),
-//     img5: ('https://i.postimg.cc/B6nVgBym/1.jpg'),
-//     img6: ('https://i.postimg.cc/B6nVgBym/1.jpg'),
-//     img7: ('https://i.postimg.cc/B6nVgBym/1.jpg'),
-//     img8: ('https://i.postimg.cc/B6nVgBym/1.jpg'),
-//     img9: ('https://i.postimg.cc/B6nVgBym/1.jpg'),
-//     img10: ('https://i.postimg.cc/B6nVgBym/1.jpg'),
-//     img11: ('https://i.postimg.cc/B6nVgBym/1.jpg'),
-//     img12: ('https://i.postimg.cc/B6nVgBym/1.jpg'),
-//     img13: ('https://i.postimg.cc/B6nVgBym/1.jpg'),
-//     img14: ('https://i.postimg.cc/B6nVgBym/1.jpg'),
-//     img15: ('https://i.postimg.cc/B6nVgBym/1.jpg'),
-//     img16: ('https://i.postimg.cc/B6nVgBym/1.jpg'),
-//     img17: ('https://i.postimg.cc/B6nVgBym/1.jpg'),
-//     img18: ('https://i.postimg.cc/B6nVgBym/1.jpg')
-
-// }
