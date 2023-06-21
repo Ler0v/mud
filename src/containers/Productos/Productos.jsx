e200627c3052153da56c24fe613e76d57a4359bd
Agustin <90871968+Ler0v@users.noreply.github.com>
2023-06-21 18:36:19 -0300

    terminada

--- src/containers/Productos/Productos.jsx
+++ src/containers/Productos/Productos.jsx
@@ -6,7 +6,7 @@ const Productos = () => {
 <>
     <h1 className='Titulo'>Productos</h1>
     <div className='seccionProductos'>
-        <h1 className='titulo'>Podes visitar nuestra tienda por este link!</h1>
+        <h1 className='titulo'>Podes visitar nuestra tienda en el siguiente enlace</h1>
         <a href="https://mudmakeupdesign.mitiendanube.com/productos/" rel='noopener' target="_blank">Tienda</a>
     </div>
 </>
